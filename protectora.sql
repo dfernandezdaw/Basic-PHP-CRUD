04853ab80ddc1ccb4e892c43324cea9c81791dad
David <dferrod1808@g.educaand.es>
2022-10-14 12:59:37 +0200

    updated protectora.sql

--- protectora.sql
+++ protectora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 30-09-2022 a las 12:18:17
+-- Tiempo de generación: 14-10-2022 a las 10:58:58
 -- Versión del servidor: 10.9.3-MariaDB-1:10.9.3+maria~ubu2204
 -- Versión de PHP: 8.0.23
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `protectora`
 --
+CREATE DATABASE IF NOT EXISTS `protectora` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `protectora`;
 
 -- --------------------------------------------------------
 
@@ -29,9 +31,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mascotas` (
   `id` int(11) NOT NULL,
-  `nombre` text NOT NULL,
-  `tipo` enum('perro','gato') NOT NULL DEFAULT 'gato'
+  `nombre` varchar(40) NOT NULL,
+  `tipo` enum('perro','gato') NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `mascotas`
+--
+
+INSERT INTO `mascotas` (`id`, `nombre`, `tipo`, `id_usuario`) VALUES
+(1, 'Yaky', 'perro', 1),
+(2, 'Toby', 'gato', NULL),
+(3, 'Rufo', 'perro', NULL);
 
 -- --------------------------------------------------------
 
@@ -41,11 +53,20 @@ CREATE TABLE `mascotas` (
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombre` text NOT NULL,
-  `apellidos` text NOT NULL,
-  `dni` varchar(40) NOT NULL,
-  `fk_id_mascota` int(11) NOT NULL
+  `nombre` varchar(40) NOT NULL,
+  `apellidos` varchar(40) NOT NULL,
+  `dni` char(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `dni`) VALUES
+(1, 'David', 'Fernández', '12345678T'),
+(2, 'Juan', 'Martin', '45609899E'),
+(4, 'Ana', 'Sánchez', '75467898P'),
+(5, 'Nuria', 'Gonzalez', '45348956T');
 
 --
 -- Índices para tablas volcadas
@@ -55,14 +76,14 @@ CREATE TABLE `usuarios` (
 -- Indices de la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `acogida` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `dni` (`dni`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -72,13 +93,23 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD CONSTRAINT `acogida` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
