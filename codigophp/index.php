14e37087c6098c7afbac438ebd9362787b76aa4b
David <dferrod1808@g.educaand.es>
2022-10-18 00:35:56 +0200

    basic login

--- codigophp/index.php
+++ codigophp/index.php
@@ -1,46 +1,53 @@
 <?php
-$servidor="db"; //Nombre del contenedor en Docker-Compose
-$usuario="protectora";
-$clave= "aitortilla";
-$bd="protectora";
-try {
-    // mysql es el gestor de Base de datos
+session_start();
+if(isset($_SESSION["usuario"])){
+    $servidor="db"; //Nombre del contenedor en Docker-Compose
+    $usuario="protectora";
+    $clave= "aitortilla";
+    $bd="protectora";
+    try {
+        // mysql es el gestor de Base de datos
     $conn = new PDO("mysql:host=$servidor;dbname=$bd", $usuario, $clave);
     // Establece los atributos de los reportes de errores
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Conexión satisfactoria";
-} catch(PDOException $e) {
-    echo ( "Error de conexión: " . $e->getMessage());
-    exit(0);
-}
+    } catch(PDOException $e) {
+        echo ( "Error de conexión: " . $e->getMessage());
+        exit(0);
+    }
 
-print("<h1>Usuarios de la protectora</h1>");
+    print("<h1>Usuarios de la protectora</h1>");
 
-$usuarios = $conn->query("SELECT * FROM usuarios");
-/* while($usuario = $usuarios->fetchObject()){
+    $usuarios = $conn->query("SELECT * FROM usuarios");
+    /* while($usuario = $usuarios->fetchObject()){
     print("<p>$usuario->nombre</p>");
     //print("<br>");
-} */
+    } */
 
-//Si en la consulta ya ha habido un fetch, no se vuelve a ejecutar. Solo se imprimen los nombres de usuario, una vez.
-$usuarios_asociativo = $usuarios->fetchAll(PDO::FETCH_ASSOC);
-foreach($usuarios_asociativo as $user){
-    print($user["nombre"]);
-    print("<br>");
-}
-
-print("<h1>Mascotas sin acoger :( </h1>");
-
-$mascotas = $conn->query("SELECT * FROM `mascotas` WHERE id_usuario IS NULL;");
-while($mascota = $mascotas->fetchObject()){
-    if($mascota->tipo == "gato"){
-        print("<p>$mascota->nombre: $mascota->tipo 🐱</p>");
-    }else{
-        print("<p>$mascota->nombre: $mascota->tipo 🐶</p>");
+    //Si en la consulta ya ha habido un fetch, no se vuelve a ejecutar. Solo se imprimen los nombres de usuario, una vez.
+    $usuarios_asociativo = $usuarios->fetchAll(PDO::FETCH_ASSOC);
+    foreach($usuarios_asociativo as $user){
+        print($user["nombre"]);
+        print("<br>");
     }
+
+    print("<h1>Mascotas sin acoger :( </h1>");
+
+    $mascotas = $conn->query("SELECT * FROM `mascotas` WHERE id_usuario IS NULL;");
+    while($mascota = $mascotas->fetchObject()){
+        if($mascota->tipo == "gato"){
+            print("<p>$mascota->nombre: $mascota->tipo 🐱</p>");
+        }else{
+            print("<p>$mascota->nombre: $mascota->tipo 🐶</p>");
+        }
     //print("<br>");
+    }
+}else{
+    header("Location: login.php");
 }
 ?>
 
-
+<form action="salir.php" method="post">
+    <input type="submit" value="Salir">
+</form>
 
---
+++ codigophp/login.php
@@ -0,0 +1,42 @@
+<?php
+
+include("conecta.php");
+
+if(isset($_POST["usuario"])){
+    session_start();
+    $usuario = $_POST["usuario"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM usuarios WHERE usuario = :usuario && password = :password";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":usuario", $usuario);
+    $stmt->bindParam(":password", $password);
+    $stmt->execute();
+
+    $data = $stmt->fetch();
+
+    if($data){
+        $_SESSION["usuario"] = $data["usuario"];
+        header("Location: index.php");
+    }else{
+        echo "Usuario o contraseña inválido";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login PHP</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="usuario" id="usuario" placeholder="Usuario">
+        <input type="password" name="password" id="password" placeholder="Contraseña">
+        <input type="submit" name="enviar" value="Iniciar sesión">
+    </form>
+</body>
+</html>
